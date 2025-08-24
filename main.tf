7834dd0a6367f8f787af1b8c6a449a6522023524
Zeeshan Pasha <zeeshanpasha93@gmail.com>
2025-08-24 23:02:40 +0530

    Modified main.tf file

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
   required_version = ">= 1.3.0"
 }
 
+provider "aws" {
+  region = var.aws_region
+}
+
 # Security Group
 resource "aws_security_group" "web_sg" {
   name_prefix = "pg-app-sg-"
@@ -67,20 +71,9 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-# Use latest Ubuntu 24.04 LTS AMI
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
-  }
-}
-
 # EC2 Instance
 resource "aws_instance" "example" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = "ami-0f918f7e67a3323f0" # Ubuntu 24.04 AMI in Mumbai
   instance_type               = var.instance_type
   key_name                    = var.key_name
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
@@ -166,3 +159,19 @@ EOF
     Environment = "Development"
   }
 }
+
+# Variables
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  default = "pg-app-key"
+}
+
+variable "github_user" {}
+variable "github_token" {}
