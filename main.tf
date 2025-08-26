4d01b1c46409dc72f633f2da9d146b36ea8d08d8
Zeeshan Pasha <zeeshanpasha93@gmail.com>
2025-08-26 17:49:30 +0530

    Modified main.tf file

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100.0"
+    }
+  }
+}
+
 # Security Group
 resource "aws_security_group" "web_sg" {
   name_prefix = "pg-app-sg-"
